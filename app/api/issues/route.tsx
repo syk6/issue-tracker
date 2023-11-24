38f2fe6fcc26d189a3ba392097a1dfd2745855c7
syk6 <sreekantakris@gmail.com>
2023-11-24 23:05:11 +0530

    extract error msg component

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,12 +1,7 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
+import { createIssueSchema } from "../../validationSchema";
 
-
-const createIssueSchema = z.object({
-    title: z.string().min(1, 'Title is required').max(255),
-    description: z.string().min(1,'Description is required')
-})
 
 export async function POST(request:NextRequest){
     const body = await request.json();
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,23 +1,23 @@
 'use client'
-import { Button, Callout, CalloutRoot, CalloutText, TextField } from '@radix-ui/themes'
+import { Button, Callout, CalloutRoot, CalloutText, Text, TextField } from '@radix-ui/themes'
 import React, { useState } from 'react'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller, Form } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { createIssueSchema } from '@/app/validationSchema';
+import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
-
-interface IssueForm{
-    title:string;
-    description:string;
-}
-
-
+type IssueForm = z.infer<typeof createIssueSchema>
 
 const NewIssuePage = () => {
   const router = useRouter();
-  const {register, control, handleSubmit} = useForm<IssueForm>();
+  const {register, control, handleSubmit, formState: { errors }} = useForm<IssueForm>({
+    resolver:zodResolver(createIssueSchema)
+  });
   const [error,setError] = useState('');
 
   return (
@@ -40,12 +40,15 @@ const NewIssuePage = () => {
         <TextField.Root>
             <TextField.Input placeholder="Title" {...register('title')} />
         </TextField.Root>
+            <ErrorMessage>
+                {errors.title?.message}
+            </ErrorMessage>
         <Controller
          name='description'
          control={control}
          render={( field ) => <SimpleMDE placeholder="Description" {...field}/>}
          />
-        
+        <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button>Submit New Issue</Button>
   </form>
   </div>
